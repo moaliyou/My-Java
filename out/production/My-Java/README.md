be13a957983e1daf5f2460e6ca6a7098880eb38f
moaliyou <moaliyou37@gmail.com>
2022-11-11 08:12:42 +0300

    Modify: added 3 more methods to get and check if the number is armstrong.

--- out/production/My-Java/README.md
+++ out/production/My-Java/README.md
@@ -1,2 +1,2 @@
 # My-Java
-In this repository I'll share my src.challenges, practice and project files
+In this repository I'll share my challenges, practices and project files
--- src/practice/conditions/ArmstrongNumber.java
+++ src/practice/conditions/ArmstrongNumber.java
@@ -4,22 +4,45 @@ public class ArmstrongNumber{
 
     public static void main(String[] args) {
         
-        var number = 371;
-        var counter = 0;
+        var number = 54748;
+
+        System.out.println(isArmstrong(number));
+
+    }
+
+    private static boolean isArmstrong(int number){
+        return getArmstrongNumber(number) == number;
+    }
+
+    private static int getArmstrongNumber(int number){
+
+        var digitsOfNumber = getNumberOfDigits(number);
+        var result = 0;
 
         while (number > 0){
 
             var remainder = number % 10;
 
-            System.out.println(remainder);
+            result += Math.pow(remainder, digitsOfNumber);
 
-            counter++;
+            number /= 10;
+        }
+
+        return result;
+
+    }
+
+    private static int getNumberOfDigits(int number){
+        var numberOfDigits = 0;
+
+        while (number > 0){
+
+            numberOfDigits++;
             number /= 10;
 
         }
 
-        System.out.println();
-        System.out.println(counter);
+        return numberOfDigits;
 
     }
 
